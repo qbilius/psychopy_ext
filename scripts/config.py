9ebc388e5862a9a9a89ea466c13fbd81dbce5e7f
Jonas Kubilius <qbilius@gmail.com>
2012-11-07 23:41:18 +0100

    removed as unnecessary any longer; some merged into other scripts in core

--- scripts/config.py
+++
@@ -1,9 +0,0 @@
-# these are default values
-# but can be overridden by specific settings in the scripts
-
-# paths to data and analyses
-
-
-# user-defined global variables
-hdr = 2 # Hemodynamic lag in TRs (not seconds)
-cmap = default # what plotting colors to use
