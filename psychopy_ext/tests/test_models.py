36e6bf487d0ab0e2a3b0bdfa3ac7f81fa41334ac
Jonas Kubilius <qbilius@gmail.com>
2013-04-02 23:28:00 +0200

    BF: correct import

--- psychopy_ext/tests/test_models.py
+++ psychopy_ext/tests/test_models.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scripts.core.models as models
+from .. import models
 
 import unittest
 
@@ -9,7 +9,7 @@ class TestHMAX(unittest.TestCase):
         out = m.run()
         fid = open('scripts/tests/lena_gaussian_matlab.txt')
         c2_matlab = np.array([float(i.strip('\n')) for i in fid.readlines()])
-        c2_python = np.around(out['C2'], decimals=5)  # matlab's output has 5 
+        c2_python = np.around(out['C2'], decimals=5)  # matlab's output has 5
                                                       # significant digits
         rms = np.mean(np.sqrt((c2_matlab - c2_python)**2))
         self.assertEqual(rms, 0)
@@ -19,7 +19,7 @@ class TestHMAX(unittest.TestCase):
         out = m.run()
         fid = open('scripts/tests/lena_gabor_matlab.txt')
         c2_matlab = np.array([float(i.strip('\n')) for i in fid.readlines()])
-        c2_python = np.around(out['C2'], decimals=5)  # matlab's output has 5 
+        c2_python = np.around(out['C2'], decimals=5)  # matlab's output has 5
                                                       # significant digits
         rms = np.mean(np.sqrt((c2_matlab - c2_python)**2))
         self.assertEqual(rms, 0)
@@ -32,7 +32,7 @@ class TestGaborJets(unittest.TestCase):
 
     def test_mag(self):
         mag_matlab = np.genfromtxt('scripts/tests/jet_mag.txt', delimiter=',')
-        mag_python = np.around(self.mag, decimals=5)  # matlab's output has 5 
+        mag_python = np.around(self.mag, decimals=5)  # matlab's output has 5
                                                       # significant digits
         rms = np.mean(np.sqrt((mag_matlab - mag_python)**2))
         self.assertTrue(rms, 0)
@@ -44,7 +44,7 @@ class TestGaborJets(unittest.TestCase):
                                                        # significant digits
         rms = np.mean(np.sqrt((phase_matlab - phase_python)**2))
         self.assertTrue(rms, 0)
-            
+
 
 if __name__ == '__main__':
     unittest.main()
