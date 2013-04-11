0c9a84e202bf4322be22ad4c0aca6768d1af05ca
Jonas Kubilius <qbilius@gmail.com>
2013-04-11 09:52:32 +0200

    BF: removed sdist_hg as unnecessary, closes #2
    The package now builds.
    Also other minor improvements.

--- setup.py
+++ setup.py
@@ -1,51 +1,24 @@
 from distutils.core import setup
-import os
-
-class sdist_hg(sdist):
-
-    user_options = sdist.user_options + [
-            ('dev', None, "Add a dev marker")
-            ]
-
-    def initialize_options(self):
-        sdist.initialize_options(self)
-        self.dev = 0
-
-    def run(self):
-        if self.dev:
-            suffix = '.dev%d' % self.get_tip_revision()
-            self.distribution.metadata.version += suffix
-        sdist.run(self)
-
-    def get_tip_revision(self, path=os.getcwd()):
-        from mercurial.hg import repository
-        from mercurial.ui import ui
-        from mercurial import node
-        repo = repository(ui(), path)
-        tip = repo.changelog.tip()
-        return repo.changelog.rev(tip)
 
 
-description = ('Extension of PsychoPy for an easier experimental setup via ' +
-                'design patterns.')
+description = ('A framework for reproducible neuroscience research')
 setup(
     name='psychopy_ext',
     version='0.4a2',
     author='Jonas Kubilius',
     author_email='qbilius@gmail.com',
-    packages=['psychopy_ext', 'psychopy_ext.test'],
-    cmdclass={'sdist': sdist_hg},
+    packages=['psychopy_ext', 'psychopy_ext.tests'],
     url='http://klab.lt/psychopy_ext/',
     license='LICENSE',
     description=description,
-    long_description=open('README.md').read(),
+    long_description=open('README').read(),
     install_requires=[
         "psychopy >= 1.6",
         "pandas >= 0.10",
         "pymvpa2 >= 2.0"
     ],
-    test_suite='nose.collector',
-    tests_require=['nose'],
+    #test_suite='nose.collector',
+    #tests_require=['nose'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
