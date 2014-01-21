d85d460a5a079e1b5000f0a1898e236ec31958ba
Jonas Kubilius <qbilius@gmail.com>
2014-01-21 13:31:34 +0100

    NF: exit if pandas or psychopy are missing
    also added svgwrite as a dependency

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
 from distutils.core import setup
+from distutils.version import LooseVersion
+import sys
 
 
 description = ('A framework for a rapid reproducible design, analysis and '
                'plotting of  experiments in neuroscience and psychology.')
 exec(open('psychopy_ext/version.py').read())
 
+# check non-pip-installable dependencies and exit gracefully if missing
+missing = []
+reqs = [('psychopy', '1.7'), ('pandas', '0.12')]
+for package, version in reqs:    
+    try:
+        imported = __import__(package)
+    except:
+        missing.append([package])
+    else:
+        found_version = getattr(imported, '__version__')
+        if LooseVersion(found_version) < LooseVersion(version):
+            missing.append([package, version, found_version])
+            
+if len(missing) > 0:
+    print
+    print '============='
+    print ('ERROR: Some required packages are missing or not up-to-date. '
+           'Please install them manually as they are often tricky to '
+           'install via pip.')
+    print
+    for miss in missing:
+        if len(miss) == 1:
+            print '%s: missing' % miss[0]
+        else:
+            print '%s: update to at least version %s (found %s)' % tuple(miss)
+            
+    print '============='
+    sys.exit(1)
+    
 setup(
     name='psychopy_ext',
     version=__version__,
@@ -18,9 +49,9 @@ setup(
     description=description,
     long_description=open('README.rst').read(),
     install_requires=[
-        "psychopy >= 1.7",
-        "pandas >= 0.12",
-        "docutils"
+        "docutils",
+        "seaborn>=.2",
+        "svgwrite"
     ],
     #test_suite='nose.collector',
     #tests_require=['nose'],
