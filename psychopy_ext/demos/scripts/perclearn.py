1ffa2cbe628050908d9944dbdd7e3717bc05c5fa
Jonas Kubilius <qbilius@gmail.com>
2014-07-22 16:20:26 +0200

    RF: repo_action after run

--- psychopy_ext/demos/scripts/perclearn.py
+++ psychopy_ext/demos/scripts/perclearn.py
@@ -85,11 +85,7 @@ class PercLearn(exp.Experiment):
             if expno != self.nsessions - 1:
                 self.show_text(pause_instr)
         self.after_exp()
-
-        if self.rp['register']:
-            self.register()
-        elif self.rp['push']:
-            self.commitpush()
+        self.repo_action()
         self.quit()
 
 class _Train(exp.Task):
