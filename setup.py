bdda5142a655d814a104b020a2a35831f064e87e
Jonas Kubilius <qbilius@gmail.com>
2014-07-22 14:15:11 +0200

    RF: seaborn now required
    because it's finally pip-installable!

--- setup.py
+++ setup.py
@@ -8,22 +8,21 @@ description = ('A framework for a rapid reproducible design, analysis and '
 exec(open('psychopy_ext/version.py').read())
 
 # required pip-installable packages
-pip_reqs = ['docutils', 'svgwrite']
+pip_reqs = ['docutils', 'svgwrite', 'seaborn>=0.3']
 # required non-pip-installable packages
-reqs = [('psychopy', '1.7'), ('pandas', '0.12')]
+reqs = [('psychopy', '1.80'), ('pandas', '0.12')]
 # recommended non-pip-installable packages
 recs = [('nibabel', None, 'fMRI analyses'),
-        ('mvpa2', '2.0', 'fMRI analyses'),
-        ('seaborn', '0.2', 'nice plots')]
+        ('mvpa2', '2.0', 'fMRI analyses')]
 
 def try_import(package, version, descr=None):
     """Looks for a requested package
     """
     missing = False
-    
+
     if version is None:
         version = '0'
-        
+
     try:
         imported = __import__(package)
     except:
@@ -32,11 +31,11 @@ def try_import(package, version, descr=None):
         found_version = getattr(imported, '__version__')
         if LooseVersion(found_version) < LooseVersion(version):
             missing = [package, version, found_version]
-            
+
     return missing
 
 # check for required dependencies and exit gracefully if missing
-missing_reqs = [try_import(*info) for info in reqs]            
+missing_reqs = [try_import(*info) for info in reqs]
 if any(missing_reqs):
     print
     print '============='
@@ -50,10 +49,10 @@ if any(missing_reqs):
                 print '%s: missing' % miss[0]
             else:
                 print '%s: update to at least version %s (found %s)' % tuple(miss)
-            
+
     print '============='
     sys.exit(1)
-     
+
 setup(
     name='psychopy_ext',
     version=__version__,
@@ -82,7 +81,7 @@ setup(
 )
 
 # check for recommendeed dependencies and suggest them at the end if missing
-missing_recs = [try_import(*info) for info in recs] 
+missing_recs = [try_import(*info) for info in recs]
 if any(missing_recs):
     print
     print '============='
@@ -98,5 +97,5 @@ if any(missing_recs):
                 print '%s: missing; used for %s' % (miss[0], miss[1])
             else:
                 print '%s: update to at least version %s (found %s)' % tuple(miss)
-            
+
     print '============='
