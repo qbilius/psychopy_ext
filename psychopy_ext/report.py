22b7b88c513a13e760c2e693f27b846b4557ce06
Jonas Kubilius <qbilius@gmail.com>
2016-03-14 06:59:42 -0400

    improved integration with exp

--- psychopy_ext/report.py
+++ psychopy_ext/report.py
@@ -5,93 +5,102 @@
 
 """Creates reports"""
 
-import sys, glob, os
-from cStringIO import StringIO
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys, glob, os, shutil
+#from cStringIO import StringIO
+
+import seaborn as sns
 
 
 class Report(object):
 
-    def __init__(self, info=None, rp=None, paths=None, actions='make', output='html'):
+    def __init__(self, info=None, rp=None, path='',
+                 imgdir='img', imgext='svg', actions='make', output='html'):
         self.info = info
         self.rp = rp
-        self.paths = paths
+        self.path = path
+        self.imgdir = imgdir
+        self.imgext = imgext
         self.actions = actions
         self.replist = []
+        src = os.path.abspath(os.path.dirname(__file__))
+        self.resources = os.path.join(src, 'resources/')
 
-    def make(self, reports=None):
-        if not os.path.isdir(self.paths['report']):
-            os.makedirs(self.paths['report'])
+    def open(self, reports=None):
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
         else:
-            for root, dirs, files in os.walk(self.paths['report']):
-                #for f in files:
-                    #try:
-                          #os.unlink(os.path.join(root, f))
-                    #except:
-                        #pass
+            for root, dirs, files in os.walk(self.path):
                 for d in dirs:
                     try:
                           shutil.rmtree(os.path.join(root, d))
                     except:
                         pass
 
-        src = os.path.dirname(__file__)
-        src = os.path.join(src, 'resources/')
-        for f in glob.glob(src+'*'):
+        for f in glob.glob(self.resources + '*'):
             if os.path.isfile(f):
                 if os.path.basename(f) != 'index.html':
                     shutil.copy2(f)
             else:
-                dst = os.path.join(self.paths['report'], os.path.basename(f))
+                dst = os.path.join(self.path, os.path.basename(f))
                 shutil.copytree(f, dst,
                     ignore=shutil.ignore_patterns('index.html'))
-        with open(src + 'index.html', 'rb') as tmp:
-            template = tmp.read().split('####REPLACE####')
-        self.htmlfile = open(self.paths['report'] + 'index.html', 'wb')
-        self.write(template[0])
+        with open(self.resources + 'index.html', 'rb') as tmp:
+            self.temp_begin, self.temp_end = tmp.read().split('####REPLACE####')
+        self.htmlfile = open(self.path + 'index.html', 'wb')
+        self.write(self.temp_begin)
 
+        # old_stdout = sys.stdout
+        # mystdout = StringIO()
+        # sys.stdout = mystdout
+        #
+        # if reports is None:
+        #     self.report()
+        # else:
+        #     for name, report in reports:
+        #         self.writeh(name, h='h1')
+        #         report.report()
 
-        old_stdout = sys.stdout
-        mystdout = StringIO()
-        sys.stdout = mystdout
-
-        if reports is None:
-            self.report()
-        else:
-            for name, report in reports:
-                self.writeh(name, h='h1')
-                report.report()
-
-        sys.stdout = old_stdout
-        self.write(template[1])
+    def close(self):
+        # sys.stdout = old_stdout
+        self.write(self.temp_end)
         self.htmlfile.close()
 
     def write(self, text):
         self.htmlfile.write(text)
 
     def writeh(self, text, h='h1'):
+        if isinstance(h, int): h = 'h' + str(h)
         self.htmlfile.write('<%s>%s</%s>\n' % (h, text, h))
 
-    def writeimg(self, names, caption=None, plt=None, win=None):
-        if isinstance(names, str):
+    def writeimg(self, names, caption=None, win=None):
+        if isinstance(names, (str, unicode)):
             names = [names]
-        fname = '_'.join(names) + '.png'
-        imgdir = self.paths['exp_root']
-        if plt is not None or win is not None:
-            if not os.path.isdir(imgdir):
-                os.makedirs(imgdir)
-            if plt is not None:
-                plt.savefig(imgdir + fname, bbox_inches='tight')
-            elif win is not None:
-                win.saveMovieFrames(imgdir + fname)
+        fname = '_'.join(names)
+        img_path = os.path.join(self.path, self.imgdir)
+        fpath = os.path.join(img_path, fname + '.' + self.imgext)
+        relpath = os.path.join(self.imgdir, fname + '.' + self.imgext)
+        if not os.path.isdir(img_path):
+            os.makedirs(img_path)
+
+        if win is not None:
+            win.saveMovieFrames(fpath)
+        else:
+            sns.plt.savefig(fpath, dpi=300, bbox_inches='tight')
+
         if caption is None:
             caption = ' '.join(names)
+
         self.htmlfile.write(
             '<figure>\n'
             '    <img src="%s" />\n'
             '    <figcaption><strong>Figure.</strong> %s</figcaption>\n'
-            '</figure>\n' % (self.imgpath + fname, caption)
+            '</figure>\n' % (relpath, caption)
             )
-        self.replist.append([anlname, self.info.copy(), self.rp.copy()])
 
     def writetable(self, agg, caption='', fmt=None):
         if fmt is None:
@@ -100,6 +109,7 @@ class Report(object):
 
         import pandas
         table = pandas.DataFrame(agg).to_html(float_format=fmt_lam)
+        table = table.replace('class="dataframe"', 'class="dataframe table"')
         self.htmlfile.write(
             '<figure>\n'
             '    <figcaption><strong>Table.</strong> %s</figcaption>\n'
